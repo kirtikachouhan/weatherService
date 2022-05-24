09d956fe2498b203b3218a0e79c6d348d8b8493b
Kirtika <kirtikachouhan@gmail.com>
2022-05-24 11:07:50 +0530

    final answer

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,48 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import WeatherComponent from './components/weatherComponent/weatherComponent';
+// import debounce from 'lodash.debounce';
 
 function App() {
-  let [pincode, updatePincode] =useState('');
+  let [pincode, updatePincode] = useState('');
+  let [tempType, updateTempType] = useState('');
+
+  const setWeatherData = (data: string) => {
+    // return debounce((data) => { return updatePincode(data)}, 100);
+    if(data.length>=6){
+      return updatePincode(data);
+    }
+  }
+
+
+  const seTempData = (data:string) => {
+    return updateTempType(data);
+  }
+
+
+
   return (
     <div className="App">
       <header className="App-header">
-          <h1>Weather Service</h1>
+        <h1>Weather Service</h1>
       </header>
-      <div className="container">
-        <div>
-          <input type="text" onChange={(event)=> {updatePincode(event.target.value)}}/>
+      <section>
+        <div className="container" >
+          <div className="container-body">
+            <input type="text" onChange={(event) => { setWeatherData(event.target.value) }} />
+            &nbsp;&nbsp;&nbsp;
+            <label htmlFor="temperature">Choose a Tempertaure Type:</label>
+            <select name="temperature" id="temperature" onChange={(event) => { seTempData(event.target.value) }}>
+              <option value="farenhite">Farenhite</option>
+              <option value="celcius" >Celcius</option>
+            </select>
+
+          </div>
+          <div className="container-weather">
+            <WeatherComponent pincodeData={pincode} tempType={tempType}></WeatherComponent>
+          </div>
         </div>
-        <WeatherComponent pincodeData={pincode}></WeatherComponent>
-      </div>
+      </section>
       <footer className="App-footer">
         Copy Right 2022
       </footer>
--- src/components/weatherComponent/weatherComponent.tsx
+++ src/components/weatherComponent/weatherComponent.tsx
@@ -1,76 +1,89 @@
 import React from "react";
 import PropType from "prop-types";
+import './weather.css'
 
 WeatherComponent.propTypes = {
-    pincodeData: PropType.string
+    pincodeData: PropType.string,
+    tempType: PropType.string
 }
-function WeatherComponent({pincodeData}: {pincodeData:string}){
- let weatherData:any = {
-    "560068": [
-        {
-            "Place": "Begur",
-            "Temperatue": "56",
-            "TemperatureDegree": "Celcius",
-            "Date": "24/05/2022"
-        },
-        {
-            "Place": "Begur",
-            "Temperatue": "54",
-            "TemperatureDegree": "Celcius",
-            "Date": "23/05/2022"
-        },
-        {
-            "Place": "Begur",
-            "Temperatue": "53",
-            "TemperatureDegree": "Celcius",
-            "Date": "22/05/2022"
-        },
-        {
-            "Place": "Begur",
-            "Temperatue": "52",
-            "TemperatureDegree": "Celcius",
-            "Date": "21/05/2022"
-        }
-    ],
-    "560067": [
-        {
-            "Place": "Begur",
-            "Temperatue": "56",
-            "TemperatureDegree": "Celcius",
-            "Date": "24/05/2022"
-        },
-        {
-            "Place": "Begur",
-            "Temperatue": "54",
-            "TemperatureDegree": "Celcius",
-            "Date": "23/05/2022"
-        },
-        {
-            "Place": "Begur",
-            "Temperatue": "53",
-            "TemperatureDegree": "Celcius",
-            "Date": "22/05/2022"
-        },
-        {
-            "Place": "Begur",
-            "Temperatue": "52",
-            "TemperatureDegree": "Celcius",
-            "Date": "21/05/2022"
-        }
-    ]
-};
+function WeatherComponent({ pincodeData, tempType }: { pincodeData: string, tempType:string }) {
+    let weatherData: any = {
+        "560068": [
+            {
+                "Place": "Begur",
+                "Temperatue": "56",
+                "TemperatureDegree": "Celcius",
+                "Date": "24/05/2022"
+            },
+            {
+                "Place": "Begur",
+                "Temperatue": "54",
+                "TemperatureDegree": "Celcius",
+                "Date": "23/05/2022"
+            },
+            {
+                "Place": "Begur",
+                "Temperatue": "53",
+                "TemperatureDegree": "Celcius",
+                "Date": "22/05/2022"
+            },
+            {
+                "Place": "Begur",
+                "Temperatue": "52",
+                "TemperatureDegree": "Celcius",
+                "Date": "21/05/2022"
+            }
+        ],
+        "560067": [
+            {
+                "Place": "Begur",
+                "Temperatue": "56",
+                "TemperatureDegree": "Celcius",
+                "Date": "24/05/2022"
+            },
+            {
+                "Place": "Begur",
+                "Temperatue": "54",
+                "TemperatureDegree": "Celcius",
+                "Date": "23/05/2022"
+            },
+            {
+                "Place": "Begur",
+                "Temperatue": "53",
+                "TemperatureDegree": "Celcius",
+                "Date": "22/05/2022"
+            },
+            {
+                "Place": "Begur",
+                "Temperatue": "52",
+                "TemperatureDegree": "Celcius",
+                "Date": "21/05/2022"
+            }
+        ]
+    };
 
+
+    const changeTempFehToCel = (feh) => {
+        return feh - 32 * 5 / 9;
+    }
 
     return (
-    <div>
-        weather Card {pincodeData}
-        {pincodeData && weatherData.pincodeData && weatherData[pincodeData].map((data:any)=> 
-            (<div key={data.Date}><div> {data.Temperatue} </div>
-            <div> {data.Place} </div>
-            <div> {data.TemperatureDegree} </div>
-            <div> {data.Date} </div></div>)
-        )}
-    </div>
+        <div className="row">
+            {
+                pincodeData && weatherData[pincodeData] ? 
+           (weatherData[pincodeData].map((data: any) =>
+                <div className="column" key={data.Date}>
+                    <div className="card">
+                       <div> Temperatue : {tempType === "farenhite" ? changeTempFehToCel(data.Temperatue) : data.Temperatue} </div>
+                        <div> Place : {data.Place} </div>
+                        <div> Date : {data.Date} </div>
+                        <br /></div>
+                </div>
+                )) :
+                    pincodeData.length  ? (<div>Invalid Pincode</div> ) : <div></div>
+            }
+
+        </div>
     );
 
 }
